3b32d3c424f8284f141d12a9c2f812e0dc907589
Xhunn Raymundo <xhunnworks@gmail.com>
2021-08-18 21:31:06 +0900

    Practicing object constructor

--- d1/index.js
+++ d1/index.js
@@ -22,3 +22,20 @@ console.log(cellphone)
 function Laptop() {
     this.weight = '500 grams';
 }
+// Disconnected //
+
+function Pokemon(name, level, type) {
+    this.pokemonName = name;
+    this.pokemonLevel = level;
+    this.pokemonHealth = 80 * level; 
+    this.pokemonType = type;
+    this.attack = function(targetPokemon){
+        console.log(this.pokemonName + " attacked " + targetPokemon.pokemonName);
+    };
+}
+
+let charmander = new Pokemon('Charmander', 8, 'Fire');
+console.log(charmander);
+
+let pikachu = new Pokemon('Pikachu', 9, 'Lightning');
+console.log(pikachu);
