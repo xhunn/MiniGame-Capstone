6677415b47c7d86d8450dd4f082625b935f7a747
Xhunn Raymundo <xhunnworks@gmail.com>
2021-08-25 17:26:55 +0900

    Finished with Menu and character Select

--- a1/index.js
+++ a1/index.js
@@ -1,11 +1,13 @@
 //Create a template for each pokemon.
 //[OJECT BLUEPRINT TEMPLATE]
-function Pokemon(name, lvl, front, back) {
+function Pokemon(name, lvl, front, back, element, basePower) {
     this.name = name;
     this.level = lvl;
     this.health = 50 * lvl;
     this.imageFront = front;
     this.imageBack = back;
+    this.element = element;
+    this.power = basePower * lvl;
 }
 
 //Create a template for each playing area.
@@ -16,9 +18,11 @@ function Battlefield(bg, name) {
 
 
 //List of playable characters. 
-const mewtwo = new Pokemon("MewTwo", 2, 'image', 'image');
-const articuno = new Pokemon('Articuno', 2, 'image', 'image');
-const blastoise = new Pokemon('Blastoise', 2, 'image', 'image')
+const mewtwo = new Pokemon("MewTwo", 5, './images/mewtwo.gif', './images/mew2back.gif', 'Psychic', 23);
+const articuno = new Pokemon('Articuno', 5, './images/articuno.gif', './images/articunoBack.gif', 'Flying, Ice', 22);
+const lugia = new Pokemon('Lugia', 5, './images/lugia.gif', './images/lugiaBack.gif', 'Flying, Psychic', 23);
+const charizard = new Pokemon('Charizard', 5, './images/charmander.gif', './images/charmanderBack.gif', 'Flying, Fire', 24);
+const venusaur = new Pokemon('Venusaur', 5, './images/venusaurFront.gif', './images/venusaurBack.gif', 'Grass, Poison', 23);
 
 //List of playing area. 
 const backyard = new Battlefield("image", "Backyard");
@@ -33,37 +37,44 @@ areas[1] = volcano;
 const characters = Array();
 characters[0] = articuno;
 characters[1] = mewtwo;
-characters[2] = blastoise;
+characters[2] = lugia;
+characters[3] = charizard;
+characters[4] = venusaur;
 
 let mainScreen = document.getElementById('game');
 
-function contestants(pokemon1, pokemon2) {
-    let contestant1 = document.getElementById('healthIndicator1');
-    let contestant2 = document.getElementById('healthIndicator2');
-
-    let pokemonName1 = pokemon1.name;
-    let pokemonName2 = pokemon2.name;
-
-    contestant1.innerHTML = `<h3 class="container pl-0 lead">Player - ${pokemonName1}</h3>`;
-    contestant2.innerHTML = `<h3 class="container pl-0 lead">Computer - ${pokemonName2}</h3>`;
+function battleUpdate(pokemon1, pokemon2) {
     
 }   
 
-function battle() {
+function battle(playerChosen) {
+    let playerPokemon = characters[playerChosen];
+    let opponentNumber = Math.ceil(Math.random() * characters.length)
+    let randomPokemon = characters[opponentNumber];
     mainScreen.innerHTML = `
         <div class="col">
-            <div class="row mt-4 text-center">
-                <h5 class="container">BATTLE!</h5>
+            <div class="row mt-4 mb-5 text-center">
+                <div class="col">
+                    <h1 class="diplay-3">BATTLE!</h1>
+                </div>
+            </div>
+            <div class="row justify-content-around">
+                <div class="col-5">
+                    <p class="lead">${playerPokemon.name}</p>
+                </div>
+                <div class="col-5 text-right">
+                    <p class="lead">${randomPokemon.name}</p>
+                </div>
             </div>
             <div class="row justify-content-around">
                 <div class="col-5">
                     <div class="progress">            
-                        <div class="progress-bar bg-success w-50"></div>
+                        <div class="progress-bar bg-success" id="playerPokemon"></div>
                     </div>
                 </div>
                 <div class="col-5">
                     <div class="progress">
-                        <div class="progress-bar bg-success w-50"></div>
+                        <div class="progress-bar bg-success" id="opponentPokemon"></div>
                     </div>
                 </div>
             </div>
@@ -71,7 +82,7 @@ function battle() {
                 <div class="col-5">
                     <div class="container lead pl-0" id="healthIndicator1"></div>
                 </div>
-                <div class="col-5">
+                <div class="col-5 text-right">
                     <div class="container lead pl-0" id="healthIndicator2"></div>
                 </div>
             </div>
@@ -81,52 +92,70 @@ function battle() {
     let player = document.getElementById('healthIndicator1');
     let opponent = document.getElementById('healthIndicator2');
     player.innerHTML = `Player`;
-    opponent.innerHTML = `Trainer Opponent`;
+    opponent.innerHTML = `Computer`;
 }
 
-function characterSelected(index) {
-    let characterScreen = document.getElementById('charcterSelectedScreen');
-    characterChosen = characters[index];
+function characterSelected(playerChosen) {
+    let characterScreen = document.getElementById('characterSelectedScreen');
+    characterChosen = characters[playerChosen];
     characterScreen.innerHTML = `
         <div class="row">
-            <div class="col">
-                <img src="${characterChosen.imageFront}" alt="No Image Found" class="img-fluid">
+            <div class="col text-center">
+                <img src="${characterChosen.imageFront}" alt="No Image Found" class="img-fluid my-5 p-5" style="height: 500px;">
             </div>
         </div>
-        <div class="row border">
-            <div class="col-6">
-                
-            </div>
-            <div class="col-6">
-            
+        <div class="row">
+            <div class="col text-center">
+                <h1 class="display-5 mb-5">${characterChosen.name}</h1>
             </div>
         </div>
-        <div class="row border">
-            <div class="col-6">
-                
+        <div class="row">
+            <div class="col-6 border">
+                <p class="lead  mb-0 mt-1">HEALTH: ${characterChosen.health}</p>
             </div>
-            <div class="col-6">
-            
+            <div class="col-6 border">
+                <p class="lead  mb-0 mt-1">LEVEL: ${characterChosen.level}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 border align-middle">
+                <p class="lead mb-0 mt-1">ELEMENT/TYPE: ${characterChosen.element}</p>
+            </div>
+            <div class="col-6 border">
+                <p class="lead  mb-0 mt-1">POWER: ${characterChosen.power}</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col text-center">
+                <button class="d-inline-flex btn btn-outline-dark align-items-center" onclick="battle(${playerChosen})"><img src="https://img.icons8.com/ios-filled/50/000000/pokeball--v2.png" style="width: 30px" /><h1 class="lead ml-2 mt-1">SELECT</h1></button>
             </div>
         </div>
     `;
 }
 
 function characterSelection() {
+    let pokemonIndexing = -1;
     let pokemonSelection = characters.map(function(element){
         for (let pokemonCount = 0; pokemonCount < characters.length; pokemonCount++) { 
+            pokemonIndexing++;
             return mainScreen.innerHTML = ` 
-                <div class="row justify-content-center my-5">
+                <div class="row justify-content-center my-4">
                     <div class="col">
-                        <button class="btn btn-dark" onmouseover="characterSelected(pokemonCount)">
+                        <button class="btn btn-outline-secondary" onclick="characterSelected(${pokemonIndexing})" style="width: 400px">
                             <h1 class="display-3">${element.name}</h1>
                         </button>
                     </div>
                 </div>
-            `;}})
+            `;
+        }})
     pokemonSelection = pokemonSelection.join(" ")
     mainScreen.innerHTML = `
         <div class="col-3 mt-5 ml-3">
+            <div class="row mb-5">
+                <div class="col text-center">
+                    <h1 class="display-3">Character Selection</h1>
+                </div>
+            </div>
             <div class="btn-toolbar justify-content-center" role="toolbar">
                 <div class="btn-group-vertical" role="group">
                     ${pokemonSelection}
@@ -134,10 +163,8 @@ function characterSelection() {
             </div>
         </div>
         <div class="col-8">
-            <div class="row">
-                <div class="col mr-3" id="characterSelectedScreen">
-
-                </div>
+            <div class="row justify-content-center">
+                <div class="col" id="characterSelectedScreen"></div>
             </div>
         </div>
     `;
